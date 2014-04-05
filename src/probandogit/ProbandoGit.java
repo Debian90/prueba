0ff40fd98d96a05847acb96c23d25f802121ed7b
Usuario-Gustavo <Usuario-Gustavo@GustavoW7>
2014-04-05 18:25:25 -0300

    comitprueba

---
+++ src/probandogit/ProbandoGit.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package probandogit;
+
+/**
+ *
+ * @author Usuario-Gustavo
+ */
+public class ProbandoGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+}
