bd993468b9b4e486c6bd206dd8584efba018d473
Debian90 <debian90@outlook.com>
2014-04-05 18:29:07 -0300

    Update ProbandoGit.java

--- src/probandogit/ProbandoGit.java
+++ src/probandogit/ProbandoGit.java
@@ -14,6 +14,6 @@ public class ProbandoGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        System.out.println("hola mundo que tal");
     }
 }
